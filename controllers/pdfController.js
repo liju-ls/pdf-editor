c4e172841772b05d77910086b9ce03769d30f023
LIJU <liju.lukas@gmail.com>
2024-03-18 23:46:42 +0530

    added pdf page order change feature and refactor file deletion code

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -1,6 +1,17 @@
 import { PDFDocument } from "pdf-lib";
 import fs from "fs";
 
+// delete input file
+function deleteFile(filePath) {
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+}
+
+// handle extract pages from pdf and
+// create and return a new pdf
 export async function extractPdfPages(req, res) {
   const pdfFilename = req.file.path;
   try {
@@ -20,8 +31,42 @@ export async function extractPdfPages(req, res) {
       const newFile = await newDoc.save();
       res.write(newFile);
       res.end();
+
+      deleteFile(pdfFilename);
     });
   } catch (err) {
-    console.log("error", err.name);
+    console.log("Error : ", err);
+  }
+}
+
+// handle change order of pages in pdf and
+// return a new pdf with changed order
+export async function changePdfOrder(req, res) {
+  const pdfFilename = req.file.path;
+  try {
+    fs.readFile(pdfFilename, async (err, pdf) => {
+      if (err) throw err;
+
+      const pdfDoc = await PDFDocument.load(pdf);
+      const newDoc = await PDFDocument.create();
+
+      const pageOrder = JSON.parse(req.body.order);
+      const allPages = pdfDoc.getPages();
+
+      const copiedPages = await newDoc.copyPages(pdfDoc, pageOrder);
+
+      for (let i = 0; i < pageOrder.length; i++) {
+        newDoc.addPage(copiedPages[i]);
+      }
+
+      const newFile = await newDoc.save();
+
+      res.write(newFile);
+      res.end();
+
+      deleteFile(pdfFilename);
+    });
+  } catch (err) {
+    console.log("Error : ", err);
   }
 }
--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { extractPdfPages } from "../controllers/pdfController.js";
+import {
+  extractPdfPages,
+  changePdfOrder,
+} from "../controllers/pdfController.js";
 import upload from "../middlewares/upload.js";
 
-const router = express.Router()
+const router = express.Router();
 
+// route to extract page
 router.post("/extract-pages", upload.single("file"), extractPdfPages);
+
+// route to change page order in pdf
+router.post("/change-order", upload.single("file"), changePdfOrder);
 
 export default router;
