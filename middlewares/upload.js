5e7a157c514044ae6c94c9b11f9d8895a0d0278e
LIJU <liju.lukas@gmail.com>
2024-03-18 23:09:05 +0530

    change file upload path

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,18 +2,20 @@ import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const uploadPath = path.join(__dirname, "../public");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, path.dirname(__filename));
-    },
-    filename: function (req, file, cb) {
-        const filename = `${Date.now()}`+ "-" + file.fieldname + ".pdf";
-        cb(null, filename);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    const filename = `${Date.now()}` + "-" + file.fieldname + ".pdf";
+    cb(null, filename);
+  },
+});
 
-const upload = multer({ storage:storage });
+const upload = multer({ storage: storage });
 
 export default upload;
