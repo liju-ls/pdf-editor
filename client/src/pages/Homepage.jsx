18f2d7cea01a3158a7eda268d05b994bdf3af3f4
LIJU <liju.lukas@gmail.com>
2024-03-25 14:05:56 +0530

    update: moved account page to homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,10 +1,21 @@
+import { useContext, useState, createContext } from "react";
 import Editor from "../components/Editor";
+import Files from "../components/Files";
+import { loggedContext } from "../App";
+
+export const fileContext = createContext();
 
 function Homepage() {
+  const [isLogged, setIsLogged] = useContext(loggedContext);
+  const [file, setFile] = useState(null);
+
   return (
-    <>
-      <Editor />
-    </>
+    <fileContext.Provider value={[file, setFile]}>
+      <div className="d-flex flex-column">
+        <Editor />
+        {isLogged && !file && <Files />}
+      </div>
+    </fileContext.Provider>
   );
 }
 
